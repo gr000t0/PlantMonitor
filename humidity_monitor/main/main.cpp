8639c4fd09c5c1dad6df7f8df626da47fa4528b7
Daniel Wenger <d.wenger@bora.com>
2025-07-31 17:00:50 +0200

    revert useless changes

--- humidity_monitor/main/main.cpp
+++ humidity_monitor/main/main.cpp
@@ -4,6 +4,7 @@
 #include "esp_console.h"
 #include "esp_system.h"
 #include "esp_err.h"
+#include "esp_vfs_dev.h"
 
 
 #include "sensor_service.hpp"
@@ -17,10 +18,16 @@ void init_console(void) {
 
 
 void start_console_repl(void) {
-    const esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-    esp_console_repl_t *repl = NULL;
-    esp_console_dev_uart_config_t dev_uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_console_new_repl_uart(&dev_uart_config, &repl_config, &repl) );
+        // 1) REPL-Konfig
+    esp_console_repl_config_t repl_cfg = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    // 2) UART-Device-Konfig
+    esp_console_dev_uart_config_t uart_cfg = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    // 3) Handle
+    esp_console_repl_t *repl = nullptr;
+
+    // 4) REPL anlegen (installiert Treiber + VFS automatisch)
+    ESP_ERROR_CHECK( esp_console_new_repl_uart(&uart_cfg, &repl_cfg, &repl) );
+    // 5) REPL starten
     ESP_ERROR_CHECK( esp_console_start_repl(repl) );
 }
 
